da96272b34e67851c3068c67f819ab0c4bc2e53b
Sabrimrn <sabri.merroun@gmail.com>
2025-11-09 00:36:56 +0100

    zelfontworpen usercontrol toegevoegd

---
+++ CineFlix_WPF/HeaderControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace CineFlix_WPF
+{
+    public partial class HeaderControl : UserControl
+    {
+        public HeaderControl()
+        {
+            InitializeComponent();
+
+            // Controleer of een gebruiker is ingelogd
+            if (App.CurrentUser != null)
+            {
+                LoggedInUserTextBlock.Text = App.CurrentUser.Email;
+            }
+            else
+            {
+                LoggedInUserTextBlock.Text = "Niet ingelogd";
+            }
+        }
+    }
+}
